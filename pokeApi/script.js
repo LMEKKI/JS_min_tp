245a951a3309cd167f09181eb61dc7ab856e818f
hlmekki <hamdani.lmekki@gmail.com>
2024-04-19 14:48:06 +0200

    ajout des img sur les nom des pokemon et quelque amelioration de la logique

--- pokeApi/script.js
+++ pokeApi/script.js
@@ -1,14 +1,23 @@
 const ulelement = document.querySelector(".listePokemon");
+const ulImages = document.querySelector(".listeImgPokemon");
+console.log(ulImages);
 
 const contactApi = async () => {
   const data = await fetch("https://pokebuildapi.fr/api/v1/pokemon/limit/20");
   const dataTransform = await data.json();
-  dataTransform.forEach((names) => {
-    var newDiv = document.createElement("li");
-    var newContent = document.createTextNode(names.name);
-    newDiv.appendChild(newContent);
 
-    ulelement.appendChild(newDiv);
+  dataTransform.forEach((names) => {
+    let newLi = document.createElement("div");
+    let newDiv = document.createElement("div");
+    let newName = document.createTextNode(names.name);
+    let newImg = document.createElement("img");
+    newDiv.appendChild(newName);
+
+    newImg.src = names.sprite;
+    newLi.appendChild(newImg);
+    newLi.appendChild(newDiv);
+
+    ulelement.appendChild(newLi);
   });
 };
 contactApi();
