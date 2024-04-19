877ca4ccd531917cc9653eb136322e135d3e67ae
hlmekki <hamdani.lmekki@gmail.com>
2024-04-20 01:01:33 +0200

    ajout de la logic suivente : recuperation d'un pokemon aleatoire pour le player et enemy a chaque click sur le boutton play

--- pokeGame/script.js
+++ pokeGame/script.js
@@ -2,20 +2,33 @@ const container = document.querySelector(".container");
 const divPlayer = document.querySelector(".player");
 const divEnemy = document.querySelector(".enemy");
 const playBtn = document.querySelector(".btnPlay");
+let LsPokemon = [];
 
 const fetchPokemon = async function () {
   const reponse = await fetch("https://pokebuildapi.fr/api/v1/random/team");
   const pokemon = await reponse.json();
 
   function launchGame() {
-    let LsPokemon = [];
     for (let index = 0; index < 2; index++) {
-      LsPokemon = pokemon[index];
+      LsPokemon.push(pokemon[index]);
     }
+    const playerName = document.createTextNode(LsPokemon[0].name);
+    const playerimg = document.createElement("img");
     console.log(LsPokemon);
+    playerimg.src = LsPokemon[0].image;
+    divPlayer.appendChild(playerimg);
+    divPlayer.appendChild(playerName);
+    // enemy
+    const enemyName = document.createTextNode(LsPokemon[1].name);
+    const enemyimg = document.createElement("img");
+    console.log(LsPokemon);
+    enemyimg.src = LsPokemon[1].image;
+    divEnemy.appendChild(enemyimg);
+    divEnemy.appendChild(enemyName);
   }
-
-  playBtn.addEventListener("submit", launchGame());
+  launchGame();
 };
 
-fetchPokemon();
+playBtn.addEventListener("click", () => {
+  fetchPokemon();
+});
