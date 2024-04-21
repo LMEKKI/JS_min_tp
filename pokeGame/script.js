3ee2aeeac478b5a131112c0a61eb74e1b0942056
hlmekki <hamdani.lmekki@gmail.com>
2024-04-21 17:41:10 +0200

    amelioration du code , ajoute des btn def et attaque

--- pokeGame/script.js
+++ pokeGame/script.js
@@ -2,38 +2,72 @@ const container = document.querySelector(".container");
 const divPlayer = document.querySelector(".player");
 const divEnemy = document.querySelector(".enemy");
 const playBtn = document.querySelector(".btnPlay");
+const attBtn = document.querySelector(".btnAtt");
+const defBtn = document.querySelector(".btnDef");
+let enemyTextName = null;
+const enemyimg = document.createElement("img");
+const enemyName = document.createElement("p");
+let playerTextName = null;
+const playerimg = document.createElement("img");
+const playerName = document.createElement("p");
 let LsPokemon = [];
+let data;
 
 const fetchPokemon = async function () {
   const reponse = await fetch("https://pokebuildapi.fr/api/v1/random/team");
   const pokemon = await reponse.json();
-
-  function launchGame() {
-    for (let index = 0; index < 2; index++) {
-      LsPokemon.push(pokemon[index]);
-    }
-    const playerTextName = document.createTextNode(LsPokemon[0].name);
-    const playerimg = document.createElement("img");
-    const playerName = document.createElement("p");
-    console.log(LsPokemon);
-    playerimg.src = LsPokemon[0].image;
-    playerName.appendChild(playerTextName);
-    divPlayer.appendChild(playerimg);
-    divPlayer.appendChild(playerName);
-    // enemy
-    const enemytextName = document.createTextNode(LsPokemon[1].name);
-    const enemyimg = document.createElement("img");
-    const enemyName = document.createElement("p");
-    console.log(LsPokemon);
-    enemyimg.src = LsPokemon[1].image;
-    enemyName.appendChild(enemytextName);
-    divEnemy.appendChild(enemyimg);
-    divEnemy.appendChild(enemyName);
-  }
-
+  data = pokemon;
   launchGame();
 };
 
 playBtn.addEventListener("click", () => {
   fetchPokemon();
 });
+
+function loadPokemon() {
+  for (let index = 0; index < 2; index++) {
+    LsPokemon.push(data[index]);
+  }
+
+  console.log(LsPokemon);
+  playerTextName = document.createTextNode(LsPokemon[0].name);
+  enemyTextName = document.createTextNode(LsPokemon[1].name);
+  playerimg.src = LsPokemon[0].image;
+  playerName.appendChild(playerTextName);
+  divPlayer.appendChild(playerimg);
+  divPlayer.appendChild(playerName);
+
+  // enemy
+
+  console.log(LsPokemon);
+  enemyimg.src = LsPokemon[1].image;
+  enemyName.appendChild(enemyTextName);
+  divEnemy.appendChild(enemyimg);
+  divEnemy.appendChild(enemyName);
+}
+
+function launchGame() {
+  playBtn.className = "hideBtn";
+  activPlayerBtn();
+  loadPokemon();
+  GameLoqic();
+}
+
+function hidePlayerBtn() {
+  attBtn.className = "hideBtn";
+  defBtn.className = "hideBtn";
+}
+function activPlayerBtn() {
+  attBtn.className = "activeBtn";
+  defBtn.className = "activeBtn";
+}
+
+function GameLoqic() {
+  attBtn.addEventListener("click", () => {
+    enemyimg.className = "red";
+    hidePlayerBtn();
+  });
+  defBtn.addEventListener("click", () => {
+    hidePlayerBtn();
+  });
+}
